25d6504e69367a1b420b3827798a94fc05dce381
Jaime Guaman <jaime@guaman.cl>
2016-01-12 13:36:25 -0300

    I need the fucking built files, fuck you bower

--- angular-masonry.js
+++ angular-masonry.js
@@ -1,3 +1,8 @@
+/*!
+ * angular-masonry 0.7.0
+ * Pascal Hartig, weluse GmbH, http://weluse.de/
+ * License: MIT
+ */
 (function () {
   'use strict';
   angular.module('wu.masonry', []).controller('MasonryCtrl', [
@@ -10,6 +15,12 @@
       var destroyed = false;
       var self = this;
       var timeout = null;
+      var mason = null;
+      this.preserveOrder = false;
+      // Masonry object created... use away
+      $scope.$on('masonry.created', function () {
+        mason = $scope.mason;
+      });
       this.scheduleMasonryOnce = function scheduleMasonryOnce() {
         var args = arguments;
         var found = schedule.filter(function filterFn(item) {
@@ -19,6 +30,8 @@
           this.scheduleMasonry.apply(null, arguments);
         }
       };
+      // Make sure it's only executed once within a reasonable time-frame in
+      // case multiple elements are removed or added at once.
       this.scheduleMasonry = function scheduleMasonry() {
         if (timeout) {
           $timeout.cancel(timeout);
@@ -29,13 +42,13 @@
             return;
           }
           schedule.forEach(function scheduleForEach(args) {
-            $element.masonry.apply($element, args);
+            mason[args]();
           });
           schedule = [];
         }, 30);
       };
-      function defaultLoaded($element) {
-        $element.addClass('loaded');
+      function defaultLoaded(_el) {
+        angular.element(_el).addClass('loaded');
       }
       this.appendBrick = function appendBrick(element, id) {
         if (destroyed) {
@@ -43,36 +56,49 @@
         }
         function _append() {
           if (Object.keys(bricks).length === 0) {
-            $element.masonry('resize');
+            mason.resize();
           }
           if (bricks[id] === undefined) {
-            defaultLoaded(element);
+            // Keep track of added elements.
             bricks[id] = true;
-            $element.masonry('appended', element, true);
-            self.scheduleMasonryOnce('reloadItems');
-            self.scheduleMasonryOnce('layout');
+            defaultLoaded(element);
+            mason.appended(element);
           }
         }
-        element.imagesLoaded(_append);
+        function _layout() {
+          // I wanted to make this dynamic but ran into huuuge memory leaks
+          // that I couldn't fix. If you know how to dynamically add a
+          // callback so one could say <masonry loaded="callback($element)">
+          // please submit a pull request!
+          self.scheduleMasonryOnce('layout');
+        }
+        if (self.preserveOrder) {
+          _append();
+          imagesLoaded(element, _layout);
+        } else {
+          imagesLoaded(element, function imagesLoaded() {
+            _append();
+            _layout();
+          });
+        }
       };
       this.removeBrick = function removeBrick(id, element) {
         if (destroyed) {
           return;
         }
         delete bricks[id];
-        $element.masonry('remove', element);
+        mason.remove(element);
         this.scheduleMasonryOnce('layout');
       };
       this.destroy = function destroy() {
         destroyed = true;
-        if ($element.data('masonry')) {
-          $element.masonry('destroy');
-        }
+        // Gently uninitialize if still present
+        mason.destroy();
         $scope.$emit('masonry.destroyed');
         bricks = [];
       };
       this.reload = function reload() {
-        $element.masonry();
+        mason.layout();
         $scope.$emit('masonry.reloaded');
       };
     }
@@ -80,14 +106,18 @@
     return {
       restrict: 'AE',
       controller: 'MasonryCtrl',
+      scope: true,
       link: {
         pre: function preLink(scope, element, attrs, ctrl) {
           var attrOptions = scope.$eval(attrs.masonry || attrs.masonryOptions);
-          var options = angular.extend(attrOptions || {}, {
+          var options = angular.extend({
               itemSelector: attrs.itemSelector || '.masonry-brick',
               columnWidth: parseInt(attrs.columnWidth, 10)
-            });
-          element.masonry(options);
+            }, attrOptions || {});
+          // Assign to scope to share with the controller
+          scope.mason = new Masonry(element[0], options);
+          var preserveOrder = scope.$eval(attrs.preserveOrder);
+          ctrl.preserveOrder = preserveOrder !== false && attrs.preserveOrder !== undefined;
           scope.$emit('masonry.created', element);
           scope.$on('$destroy', ctrl.destroy);
         }
